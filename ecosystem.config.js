0d4baa5f9e705c0f704ef50f569b7d773c87a2c7
Nget Tim <ngettim14@gmail.com>
2025-09-05 09:02:47 +0700

    update ecosystem.config.js

--- ecosystem.config.js
+++ ecosystem.config.js
@@ -1,35 +1,32 @@
 module.exports = {
   apps : [{
-    name: 'sv-invoice-scanner', // A name for your application
-    script: 'dist/main.js',    // Your NestJS application's entry point after compilation
-    instances: 'max',          // Max instances based on CPU cores
-    exec_mode: 'cluster',      // Run in cluster mode for better performance
-    watch: false,              // Set to true for development, false for production
+    name: 'sv-scanner', 
+    script: 'dist/main.js',  
+    instances: 'max',         
+    exec_mode: 'cluster',   
+    watch: false,             
     env_production: {
       NODE_ENV: 'production',
-      // Note: PORT and other app-specific variables will be loaded from the .env file
-      // that we're creating in the post-deploy script.
-      // You can still define app-specific PM2 env vars here if they are *not* in .env
     }
   }],
 
   deploy : {
     production : {
-      user : 'timdev', // Your SSH username
-      host : '38.242.149.46', // Your server's IP address or hostname
-      ref  : 'origin/master', // The Git branch to deploy from
-      repo : 'https://github.com/mrtimdev/sv_invoice_scanner_nestjs.git', // Your Git repository URL
-      path : '/home/timdev/projects/timdev/sv_invoice_scanner_nestjs', // The deployment path on your server
-      'pre-deploy-local': 'echo "Starting deployment..."', // Command to run on your local machine before deployment
+      user : 'deverloper',
+      host : '192.168.1.249', 
+      ref  : 'origin/master', 
+      repo : 'https://github.com/mrtimdev/sv_invoice_scanner_nestjs.git', 
+      path : '/home/deverloper/sv_scanner',
+      'pre-deploy-local': 'echo "Starting deployment..."',
       'post-deploy': `
-        cd /home/timdev/projects/timdev/sv_invoice_scanner_nestjs/source && \
+        cd /home/deverloper/sv_scanner && \
         npm install && \
         npm run build && \
         pm2 reload ecosystem.config.js --env production
       `,
 
 
-      'pre-setup': 'echo "Setting up server..."' // Command to run on the server before the first deploy
+      'pre-setup': 'echo "Setting up server..."'
     }
   }
 };
